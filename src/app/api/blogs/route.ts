601c0eef6d4f5bc8464b5959274f534efdc6322c
AronLEEdev <1455832454@qq.com>
2025-04-03 16:00:11 +0800

    导入blogs数据

--- src/app/api/blogs/route.ts
+++ src/app/api/blogs/route.ts
@@ -2,6 +2,6 @@ import { NextResponse } from "next/server";
 import { prisma } from "../../../../lib/prisma";
 import mock from "../../../../mock.json";
 export async function GET() {
-  // return NextResponse.json(await prisma.blog.findMany());
-  return Promise.resolve(NextResponse.json(mock));
+  return NextResponse.json(await prisma.blog.findMany());
+  // return Promise.resolve(NextResponse.json(mock));
 }
